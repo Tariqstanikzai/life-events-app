0ab8f8e39bad51c4094271d473980a15b3e94d47
Tariq Stanikzai <98728769+Tariqstanikzai@users.noreply.github.com>
2022-07-23 16:56:50 -0400

    add index file

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+// import babel from 'babel';
+
+
+import postRoutes from './routes/posts.js'; 
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://Ahmad:stanikzai@cluster0.hgt4r.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 8000;
+
+
+
+
+
+mongoose.connect(CONNECTION_URL, { useNewUrlparser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log('Server running on port: ${PORT}')))
+    .catch((error) => console.log(error.message));
+
+mongoose.set;
+
+
+
