b69a0398f472d8036665091cc761dc574721096d
Tariq Stanikzai <98728769+Tariqstanikzai@users.noreply.github.com>
2022-08-03 16:41:10 -0400

    updates

---
+++ client/src/styles.js
@@ -0,0 +1,18 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  appBar: {
+    borderRadius: 60,
+    margin: '30px 0',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'rig',
+    alignItems: 'center',
+  },
+  heading: {
+    color: '#231955',
+  },
+  image: {
+    marginLeft: '30px',
+  },
+}));
